930d5b24b5ecdd777860a363b0c819a9b002097c
QuanDinh157 <lachongquan12@gmail.com>
2026-04-07 21:33:53 +0700

    Cap nhat server

--- server/server.js
+++ server/server.js
@@ -13,17 +13,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "http://localhost:3001",
-      "http://127.0.0.1:3001",
-    ],
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ limit: "16mb", extended: true }));
