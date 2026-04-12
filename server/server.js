1ebbfb0b657ae42a5d5d3ae8cac4bf20934153ce
QuanDinh157 <lachongquan12@gmail.com>
2026-04-12 20:39:02 +0700

    Fix

--- server/server.js
+++ server/server.js
@@ -16,18 +16,7 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://dingzcustomer.onrender.com",
-    "https://dingzadmin.onrender.com",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-access-token", "token"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ limit: "16mb", extended: true }));
