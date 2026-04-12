e176d8875e17c7e85e1f12f50ce3095588c1bedb
QuanDinh157 <lachongquan12@gmail.com>
2026-04-12 22:17:30 +0700

    Fix loi port customer

--- client-customer/src/App.js
+++ client-customer/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MainComponent from "./components/MainComponent";
+import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://99dd6ad2-d9a2-49c6-9433-2fa2f6669151-00-19pg7engwesf5.sisko.replit.dev";
 
 class App extends Component {
   render() {
